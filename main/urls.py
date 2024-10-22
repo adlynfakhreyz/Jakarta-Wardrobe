24e646ff7310912a99a29494b3ae1f98f925bf4d
Dara <darazapri@gmail.com>
2024-10-22 17:49:49 +0700

    menambahkan fungsi login, register, dan logout

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, login_user, logout_user, register
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', register, name='register'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,43 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from main.models import ItemEntry
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 import uuid
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     return render(request, 'main.html')
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
