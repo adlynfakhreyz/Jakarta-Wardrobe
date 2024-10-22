3878217b697d7bb756e32be1e195bce1e1347c06
ramaditya31 <rama.aditya31@ui.ac.id>
2024-10-22 22:46:36 +0700

    coba dulu

--- main/models.py
+++ main/models.py
@@ -13,27 +13,3 @@ class ItemEntry:
     kategories = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
-RATING = [
-    (1, '$'),
-    (2, '$$'),
-    (3, '$$$'),
-    (4, '$$$$'),
-    (5, '$$$$$')
-]
-
-class Rating(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    product_id = models.ForeignKey(ItemEntry, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating_value = models.IntegerField(null=True, blank=True, choices = RATING)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.user.username
-    
-class Rate(models.Model):
-    source=models.CharField(max_length=50)
-    rating=models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.source
