00ababd039e0d06e139042364b4c663fa0a97773
adlynfakhreyz <adlynuniacc@gmail.com>
2024-10-23 13:10:50 +0700

    add some add delete function

--- user_choices/views.py
+++ user_choices/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from user_choices.models import UserChoice
+
 
 # Create your views here.
 def show_user_choices(request):
     return render(request, 'user_choices.html')
+
+def delete_user_choices(request, id):
+    pass
+    
+def add_user_choices(request, id):
+    new_user_choice = UserChoice(user_id=id, choice=request.POST['choice'])
