f34ff89aa7e588b46d35f9ba7cd349748ce8a101
SalomoImmanuel <https://github.com/SalomoManullang/Goodsify.git>
2024-10-23 14:20:02 +0700

    product card

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from products.views import show_products_by_price
+from . import views
 
 app_name = 'products'
 
 urlpatterns = [
     path('', show_products_by_price, name='show_products_by_price'),
+    path('detail/<uuid:product_id>/', views.product_detail, name='product_detail'),
 ]
