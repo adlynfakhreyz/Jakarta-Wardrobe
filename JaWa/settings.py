4ce84ee0a1550459afe1478d65f2fa80413e6823
sayyidtrq <sayyidthariq02@gmail.com>
2024-12-22 00:43:36 +0700

    deploy ulang

--- JaWa/settings.py
+++ JaWa/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from JaWa.wsgi import application
+
+
+app = application
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
