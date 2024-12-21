5f9dbd3cb9e2895c2d95178ca1c62fb87c2e8c48
adlynfakhreyz <adlynuniacc@gmail.com>
2024-12-22 01:52:00 +0700

    Attempt to deploy

--- JaWa/settings.py
+++ JaWa/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from urllib.parse import urlparse
-import dj_database_url
 from os import getenv
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
