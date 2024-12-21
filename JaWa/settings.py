68fd0a9864faa62c6c30591600cc2e6a7f12a69c
adlynfakhreyz <adlynuniacc@gmail.com>
2024-12-22 03:59:36 +0700

    Attempt to deploy

--- JaWa/settings.py
+++ JaWa/settings.py
@@ -157,7 +157,7 @@ if DEBUG:
         BASE_DIR / 'static' # merujuk ke /static root project pada mode development
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static') # merujuk ke /static root project pada mode production
+    STATIC_ROOT = BASE_DIR / 'staticfiles' # merujuk ke /static root project pada mode production
     
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
