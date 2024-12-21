4b59a4f341332540282521d6c27bbdb3953dcfa5
adlynfakhreyz <adlynuniacc@gmail.com>
2024-12-22 03:44:27 +0700

    Attempt to deploy

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
-python3 -m venv env
-env\Scripts\activate
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py import_products
