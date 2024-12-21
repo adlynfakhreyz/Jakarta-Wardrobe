ee8c97df9a13af20c01fdacef39b114fd27a990f
adlynfakhreyz <adlynuniacc@gmail.com>
2024-12-22 03:25:18 +0700

    Attempt to deploy

--- JaWa/settings.py
+++ JaWa/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = 'django-insecure-1-uvrhew1=b)cwklcyh%92ck%1n$j^cb9iplbwo7ccq$h+6x%*
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv("PRODUCTION", False)
-PROUCTION = True
 DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = [
@@ -158,7 +157,7 @@ if DEBUG:
         BASE_DIR / 'static' # merujuk ke /static root project pada mode development
     ]
 else:
-    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
+    STATIC_ROOT = BASE_DIR / 'staticfiles' # merujuk ke /static root project pada mode production
     
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
