495b774025ce51d6c3877a731977b6c334108a39
adlynfakhreyz <adlynuniacc@gmail.com>
2024-12-22 14:07:02 +0700

    chore: updata settings

--- JaWa/settings.py
+++ JaWa/settings.py
@@ -165,3 +165,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://andi-muhammad37-jawaapp.pbp.cs.ui.ac.id",
+]
